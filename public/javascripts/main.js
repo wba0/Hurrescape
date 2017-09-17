c5c2d53531be5c2f8c8d682de6112ce7ccf16396
William <william.bonneyache@gmail.com>
2017-09-16 22:20:20 -0400

    windy & wu integration start

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,12 +4,14 @@
 // });
 
 
-// $(document).ready(function($) {
-//   $.ajax({
-//   url : "http://api.wunderground.com/api/f7b22f01665f4002/currenthurricane/view.json",
-//   dataType : "jsonp",
-//   success : function(parsed_json) {
-//     console.log(parsed_json);
-//   }
-//   });
-// });
+$(document).ready(function($) {
+  $.ajax({
+  url : "http://api.wunderground.com/api/f7b22f01665f4002/currenthurricane/view.json",
+  dataType : "jsonp",
+  success : function(parsed_json) {
+    console.log(parsed_json);
+    $("#wu-map").html(parsed_json.currenthurricane["0"].stormInfo.wuiurl);
+    $("#wu-map-img").attr("src", `http://icons.wunderground.com/data/images/${parsed_json.currenthurricane["0"].stormInfo.stormNumber}_5day.gif`);
+  }
+  });
+});
