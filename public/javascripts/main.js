ac79716bc5aabf5e81b1a4b8a1d48663ef2f0ce5
wba0 <william.bonneyache@gmail.com>
2017-09-20 10:02:43 -0400

    logo and styling

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -30,7 +30,7 @@ $(document).ready(function($) {
         const hWindGusts = hurricane.Current.WindGust.Mph;
 
         const mapUrl = `http://icons.wunderground.com/data/images/${hNumber}_5day.gif`;
-        if (hCat >= 1) {
+        if (hCat >= 0) {
           $("#windy-container").append(`<iframe class="windy-iframe windy-${hNumber}" width="800" height="480"
           src="https://embed.windy.com/embed2.html?lat=${hLat}&lon=${hLng}&zoom=5&level=surface&overlay=wind&menu=true&message=&marker=&forecast=12&calendar=now&location=coordinates&type=map&actualGrid=&metricWind=kt&metricTemp=%C2%B0F" frameborder="0">
           </iframe>`);
