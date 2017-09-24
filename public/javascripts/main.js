7407cd9a9ca3af8f03c0119f6119533833848958
William <william.bonneyache@gmail.com>
2017-09-24 17:49:56 -0400

    windy iframe responsive

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -55,7 +55,7 @@ $(document).ready(function($) {
         const mapUrl = `http://icons.wunderground.com/data/images/${hNumber}_5day.gif`;
         //restrict to hurricanes conditional
         // if (hCat >= 0) {
-          $("#windy-container").append(`<iframe class="windy-iframe windy-${hNumber}" width="750" height="480"
+          $("#windy-container").append(`<iframe class="embed-responsive-item windy-iframe windy-${hNumber}"
           src="https://embed.windy.com/embed2.html?lat=${hLat}&lon=${hLng}&zoom=5&level=surface&overlay=wind&menu=true&message=&marker=&forecast=12&calendar=now&location=coordinates&type=map&actualGrid=&metricWind=kt&metricTemp=%C2%B0F" frameborder="0">
           </iframe>`);
         // }
